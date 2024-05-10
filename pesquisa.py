7048da795e0500233a33a8a6d34198803fe5c5ec
Alvaro Adriano Beck <111699313+aabeck33@users.noreply.github.com>
2024-05-10 19:13:08 -0300

    Add files via upload

--- pesquisa.py
+++ pesquisa.py
@@ -1,12 +1,26 @@
-from azure.ai.textanalytics import TextAnalyticsClient
+from azure.ai.textanalytics import (
+        TextAnalyticsClient,
+        AnalyzeSentimentAction,
+        ExtractKeyPhrasesAction,
+        ExtractiveSummaryAction
+    )
 from azure.core.credentials import AzureKeyCredential
 from azure.storage.blob import BlobServiceClient
-import PyPDF2
+import pypdf as PyPDF2
+import os
 from tokens import LANGUAGE_KEY, LANGUAGE_ENDPOINT, STORAGE_CONNECTION_STRING, BLOB_CONTAINER_NAME
+
+
+TEMP_PDF='temp.pdf'
+
+def cleanning():
+    os.remove(TEMP_PDF)
 
 
 # Read the PDF files on the Stora Account
 def pdf_read():
+    text = ""
+
     # Storage Blob Client
     blob_service_client = BlobServiceClient.from_connection_string(STORAGE_CONNECTION_STRING)
     container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
@@ -17,15 +31,15 @@ def pdf_read():
         blob_client = blob_service_client.get_blob_client(container=BLOB_CONTAINER_NAME, blob=blob_name)
 
         # Download PDF from Blob Storage
-        with open("temp.pdf", "wb") as download_file:
+        with open(TEMP_PDF, "ab") as download_file:
             download_file.write(blob_client.download_blob().readall())
 
         # Read and extract the text from PDF
-        with open("temp.pdf", "rb") as file:
+        with open(TEMP_PDF, "rb") as file:
             reader = PyPDF2.PdfReader(file)
-            text = ""
             for page in range(len(reader.pages)):
                 text += reader.pages[page].extract_text()
+
     return text
 
 
@@ -35,11 +49,84 @@ def authenticate_client():
     text_analytics_client = TextAnalyticsClient(
             endpoint=LANGUAGE_ENDPOINT, 
             credential=ta_credential)
+
     return text_analytics_client
+
+
+def document_analysis(client, text):
+    documents = [text]
+
+    poller = client.begin_analyze_actions(
+        documents,
+        display_name="Text Analysis",
+        actions=[
+            AnalyzeSentimentAction(),
+            ExtractKeyPhrasesAction(),
+            ExtractiveSummaryAction(max_sentence_count=4)
+        ],
+    )
+    document_results = poller.result()
+
+    for doc, action_results in zip(documents, document_results, print_text):
+        if print_text:
+            print(f"\nDocument text: {doc}")
+        for result in action_results:
+            if result.kind == "EntityRecognition":
+                print("...Results of Recognize Entities Action:")
+                for entity in result.entities:
+                    print(f"......Entity: {entity.text}")
+                    print(f".........Category: {entity.category}")
+                    print(f".........Confidence Score: {entity.confidence_score}")
+                    print(f".........Offset: {entity.offset}")
+
+            elif result.kind == "PiiEntityRecognition":
+                print("...Results of Recognize PII Entities action:")
+                for pii_entity in result.entities:
+                    print(f"......Entity: {pii_entity.text}")
+                    print(f".........Category: {pii_entity.category}")
+                    print(f".........Confidence Score: {pii_entity.confidence_score}")
+
+            elif result.kind == "KeyPhraseExtraction":
+                print("...Results of Extract Key Phrases action:")
+                print(f"......Key Phrases: {result.key_phrases}")
+
+            elif result.kind == "EntityLinking":
+                print("...Results of Recognize Linked Entities action:")
+                for linked_entity in result.entities:
+                    print(f"......Entity name: {linked_entity.name}")
+                    print(f".........Data source: {linked_entity.data_source}")
+                    print(f".........Data source language: {linked_entity.language}")
+                    print(
+                        f".........Data source entity ID: {linked_entity.data_source_entity_id}"
+                    )
+                    print(f".........Data source URL: {linked_entity.url}")
+                    print(".........Document matches:")
+                    for match in linked_entity.matches:
+                        print(f"............Match text: {match.text}")
+                        print(f"............Confidence Score: {match.confidence_score}")
+                        print(f"............Offset: {match.offset}")
+                        print(f"............Length: {match.length}")
+
+            elif result.kind == "SentimentAnalysis":
+                print("...Results of Analyze Sentiment action:")
+                print(f"......Overall sentiment: {result.sentiment}")
+                print(
+                    f"......Scores: positive={result.confidence_scores.positive}; \
+                    neutral={result.confidence_scores.neutral}; \
+                    negative={result.confidence_scores.negative} \n"
+                )
+
+            elif result.is_error is True:
+                print(
+                    f"...Is an error with code '{result.error.code}' and message '{result.error.message}'"
+                )
+
+        print("------------------------------------------")
+
+
 
 # Detecting sentiment and opinions in text
 def sentiment_analysis_with_opinion_mining(client, text, detaild_report):
-
     documents = [text]
 
     result = client.analyze_sentiment(documents, show_opinion_mining=True)
@@ -84,8 +171,18 @@ def sentiment_analysis_with_opinion_mining(client, text, detaild_report):
                 print("\n")
         print("\n")
 
+        result = client.extract_key_phrases(documents)
+        for idx, doc in enumerate(result):
+            if not doc.is_error:
+                print("Key phrases in article #{}: {}".format(
+                    idx + 1,
+                    ", ".join(doc.key_phrases)
+                ))
+
 
 if __name__ == '__main__':
     client = authenticate_client()
     text = pdf_read()
-    sentiment_analysis_with_opinion_mining(client, text, detaild_report=False)
+    #sentiment_analysis_with_opinion_mining(client, text, detaild_report=False)
+    document_analysis(client, text, print_text=False)
+    cleanning()
